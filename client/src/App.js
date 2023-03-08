bf367fbc94b9e6cdab427e27ba7f26b69f323072
KGjavier <116789894+KGjavier@users.noreply.github.com>
2023-03-08 06:25:38 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Login from './component/Login';
 import Registration from './component/Registration';
+import Profile from './component/ProfilePage';
+import FriendProfile from './component/FriendProfile';
+import EditProfile from './component/EditProfile';
+import UserFeed from './component/UserFeed';
 import './App.css';
 
 function App() {
@@ -10,6 +14,10 @@ function App() {
       <Routes>
           <Route path="/" element={<Login/>} />
           <Route path="/Registration" element={<Registration/>} />
+          <Route path="/Profile" element={<Profile/>} />
+          <Route path="/EditProfile" element={<EditProfile/>} />
+          <Route path="/Profile/:id" element={<FriendProfile/>} />
+          <Route path="/UserFeed" element={<UserFeed/>} />
       </Routes>
     </div>
   );
