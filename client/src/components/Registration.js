379e0ce525a14047aaf8233bd1a95d74fe8463b2
KGjavier <116789894+KGjavier@users.noreply.github.com>
2023-03-08 05:50:15 -0600

    Update Registration.js

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,1 +1,103 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
+const Registration = (props) => {
+    const [fname, setFname] = useState("");
+    const [lname, setLname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmpw, setConfirmPw] = useState("");
+    const navigate = useNavigate();
+    const [error, setError] = useState([]);
+    
+    const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const newUser = {
+        fname,
+        lname,
+        email,
+        password,
+        confirmpw,
+    };
+
+    axios
+        .post(`http://localhost:8000/api/user`, newUser, {withCredentials: true,})
+        .then((res) => {
+            console.log(res);
+            console.log(res.data.newUser);
+
+    })
+        .catch((err) => {
+            console.log(err.response.data.errors);
+            
+        
+        });
+    };
+  return (
+    <div>
+        <div className="reg__container">
+            <Link className="button__ToLogin" to="/Login">Login</Link>
+            <form className="regform" onSubmit={onSubmitHandler}>
+                <h1 className="heading__reg">Registration Form</h1>
+
+                <div>
+                    <label>First Name:*</label>
+                    <input
+                        onChange={(e) => setFname(e.target.value)}
+                        type="text"
+                        value={fname}
+                        name="fname"
+                    />
+                </div>
+
+                <div>
+                    <label>Last Name:*</label>
+                    <input
+                        onChange={(e) => setLname(e.target.value)}
+                        type="text"
+                        value={lname}
+                        name="lname"
+                    />
+                </div>
+
+                <div>
+                    <label>Email:*</label>
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="text"
+                        value={email}
+                        name="email"
+                    />
+                </div>
+
+                <div>
+                    <label>Password:*</label>
+                    <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        value={password}
+                        name="password"
+                    />
+                </div>
+
+                <div>
+                    <label>ConfirmP:*</label>
+                    <input
+                        onChange={(e) => setConfirmPw(e.target.value)}
+                        type="password"
+                        value={confirmpw}
+                        name="cpassword"
+                    />
+                </div>
+
+                <input className="Create" type="submit" value="Create Account" />
+                <p className="note">* Indicates a require field</p>
+            </form>
+        </div>
+      </div>
+);
+};
+
+export default Registration;
