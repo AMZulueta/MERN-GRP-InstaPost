d9be994ff63555cbe134998f1542520dc3edddcf
AMZulueta <117546228+AMZulueta@users.noreply.github.com>
2023-03-11 11:22:17 -0800

    Add files via upload

---
+++ client/src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main: "#f44336",
+            light: "#ce93d8"
+        },
+        secondary:{
+            main: "#f57c00",
+        },
+        otherColor: {
+            main: "#e57373"
+        }
+    }
+})
