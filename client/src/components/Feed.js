39518fe23df1294c545e2f9e1473971f12e2df79
AMZulueta <117546228+AMZulueta@users.noreply.github.com>
2023-03-12 12:12:26 -0700

    update edit in desktop

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,9 +1,45 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Card, CardHeader } from "@mui/material";
 
 const Feed = () => {
     return (
         <Box bgcolor='lightblue' flex={4} p={2}>Feed</Box>
+        <Card>
+        <CardHeader
+            avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                R
+            </Avatar>
+            }
+            action={
+            <IconButton aria-label="settings">
+                <MoreVertIcon />
+            </IconButton>
+            }
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
+        />
+        <CardMedia
+            component="img"
+            height="194"
+            image="/static/images/cards/paella.jpg"
+            alt="Paella dish"
+        />
+        <CardContent>
+            <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to cook
+            together with your guests. Add 1 cup of frozen peas along with the mussels,
+            if you like.
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+            <ShareIcon />
+            </IconButton>
+        </Card>
     );
 };
 
