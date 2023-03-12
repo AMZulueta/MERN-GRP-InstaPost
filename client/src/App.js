abf8000a000409131a8ffa7599ef7172e97df554
itsmeJan <99030599+jfbaldoza@users.noreply.github.com>
2023-03-13 00:42:34 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import React from 'react';
+import { useState } from 'react'
 import LoginReg from './components/LoginReg';
 import Profile from './components/ProfilePage';
 import FriendProfile from './components/FriendProfile';
@@ -8,14 +8,17 @@ import UserFeed from './components/UserFeed';
 import './App.css';
 
 function App() {
+
+  const [Authorized, setAuthorized] = useState("")
+
   return (
     <div className="App">
       <Routes>
-          <Route path="/" element={<LoginReg/>} />
-          <Route path="/Profile" element={<Profile/>} />
-          <Route path="/EditProfile" element={<EditProfile/>} />
-          <Route path="/Profile/:id" element={<FriendProfile/>} />
-          <Route path="/UserFeed" element={<UserFeed/>} />
+          <Route path="/" element={<LoginReg Authorized={Authorized} setAuthorized={setAuthorized} />} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/edit" element={<EditProfile/>} />
+          <Route path="/profile/:id" element={<FriendProfile/>} />
+          <Route path="/userfeed" element={<UserFeed/>} />
       </Routes>
     </div>
   );
