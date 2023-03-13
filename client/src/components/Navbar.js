2cac84b1f212f388e5bf8ef67168a788ed034b49
KGjavier <116789894+KGjavier@users.noreply.github.com>
2023-03-13 00:05:39 -0500

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,82 +1,33 @@
-import React, { useState } from "react";
-import { AppBar, Avatar, Badge, Box, InputBase, Menu, MenuItem, styled, Toolbar, Typography } from "@mui/material";
-import InstagramIcon from '@mui/icons-material/Instagram';
-import Notifications from "@mui/icons-material/Notifications";
-import Mail from "@mui/icons-material/Mail";
+import { Component } from "react";
+import { Link } from 'react-router-dom';
+import "./Navbar.css"
 
-const Search = styled('div')(({ theme }) => ({
-    backgroundColor: 'white',
-    padding: '0 10px',
-    borderRadius: theme.shape.borderRadius,
-    width: '50%'
-}));
 
-const Icons = styled(Box)(({ theme }) => ({
-    display: 'none',
-    alignItems: 'center',
-    gap: '20px',
-    [theme.breakpoints.up('sm')]:{
-        display: 'flex'
+class Navbar extends Component{
+
+    state = { clicked: false };
+    handleclick = () => {
+        this.setState({clicked: !this.state.clicked})
     }
-}));
 
-const UserBox = styled(Box)(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    gap: '5px',
-    [theme.breakpoints.up('sm')]:{
-        display: 'flex'
+    render() {
+        return (
+            <div>
+                <nav className="navbaritems">
+                    <h1 className="nav__logo"><span className="instalogo">InstaPost</span> </h1>
+                    <input className="nav__sort" placeholder="Search"/>
+                    <div className="menu__icons" onClick={this.handleclick}>
+                        <i className={this.state.clicked ? "fa fa-times" : "fa fa-bars" }></i>
+                    </div>
+                   
+                    <ul className={this.state.clicked ? "nav__menu active" : "nav__menu"}>
+                        <li><Link className="nav__links" to="/Userfeed">Home</Link></li>
+                        <li><Link className="nav__links" to="/Profile">MyProfile</Link></li>
+                        <li><Link className="nav__button" to="/Logout">Logout</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        )
     }
-}));
-
-
-const Navbar = () => {
-    const [ open, setOpen ] = useState(false);
-    return (
-        <AppBar position='sticky'> 
-            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between'}}>
-                <Typography variant='h6' sx={{ display: { xs: 'none', sm: 'block'} }}
-                >
-                    InstaPost
-                </Typography>
-                <InstagramIcon sx={{ display: { xs: 'block', sm: 'none'} }} />
-                <Search><InputBase placeholder='search...'/></Search>
-                <Icons>
-                <Badge badgeContent={4} color='error'>
-                        <Mail />
-                    </Badge>
-                    <Badge badgeContent={2} color='error'>
-                        <Notifications />
-                    </Badge>
-                </Icons>
-                <UserBox>
-                    <Avatar sx={{ width: 30, height: 30}}
-                    src=''
-                    onClick = {e=>setOpen(true)}
-                    />
-                    <Typography variant="span">Aileen</Typography>
-                </UserBox>
-            </Toolbar>
-            <Menu
-                id="demo-positioned-menu"
-                aria-labelledby="demo-positioned-button"
-                open={open}
-                onClose={e=>setOpen(false)}
-                anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-                }}
-                transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-                }}
-            >
-                <MenuItem>Profile</MenuItem>
-                <MenuItem>My account</MenuItem>
-                <MenuItem>Logout</MenuItem>
-        </Menu>
-        </AppBar>
-    );
-};
-
+}
 export default Navbar;
