7f9c8b431779d5d654155b4639c5dbc0bb6f4f71
KGjavier <116789894+KGjavier@users.noreply.github.com>
2023-03-13 00:08:44 -0500

    Create Navbar2.js

---
+++ client/src/components/Navbar2.js
@@ -0,0 +1,33 @@
+import { Component } from "react";
+import { Link } from 'react-router-dom';
+import "./Navbar.css"
+
+
+class Navbar2 extends Component{
+
+    state = { clicked: false };
+    handleclick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className="navbaritems">
+                    <h1 className="nav__logo"><span className="instalogo">InstaPost</span> </h1>
+                    
+                    <div className="menu__icons" onClick={this.handleclick}>
+                        <i className={this.state.clicked ? "fa fa-times" : "fa fa-bars" }></i>
+                    </div>
+                    
+                    <ul className={this.state.clicked ? "nav__menu active" : "nav__menu"}>
+                        <li><Link className="nav__links" to="/Userfeed">Home</Link></li>
+                        <li><Link className="nav__links" to="/Profile">MyProfile</Link></li>
+                        <li><Link className="nav__button" to="/Logout">Logout</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
+export default Navbar2;
