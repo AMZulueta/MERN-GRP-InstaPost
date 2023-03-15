e4ad6e0ef1d5fdff02f76d4d3c76027e02344361
KGjavier <116789894+KGjavier@users.noreply.github.com>
2023-03-14 23:26:15 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import Profile from './components/ProfilePage';
 import FriendProfile from './components/FriendProfile';
 import EditProfile from './components/EditProfile';
 import UserFeed from './components/UserFeed';
+import EditPost from './components/EditPost';
 import './App.css';
+
 
 function App() {
 
@@ -14,11 +16,12 @@ function App() {
   return (
     <div className="App">
       <Routes>
-          <Route path="/" element={<LoginReg Authorized={Authorized} setAuthorized={setAuthorized} />} />
-          <Route path="/profile" element={<Profile/>} />
-          <Route path="/edit" element={<EditProfile/>} />
-          <Route path="/profile/:id" element={<FriendProfile/>} />
-          <Route path="/userfeed" element={<UserFeed/>} />
+        <Route path="/" element={<LoginReg Authorized={Authorized} setAuthorized={setAuthorized} />} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/edit" element={<EditProfile/>} />
+        <Route path="/profile/:id" element={<FriendProfile/>} />
+        <Route path="/userfeed" element={<UserFeed />} />
+        <Route path="/post/edit/:id" element={<EditPost />} />
       </Routes>
     </div>
   );
