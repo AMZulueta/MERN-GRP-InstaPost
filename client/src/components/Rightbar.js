6b5531f3fc506bfdcda4c9946e2d0b78844e03c5
AMZulueta <117546228+AMZulueta@users.noreply.github.com>
2023-03-14 19:17:59 -0700

    Add files via upload

--- client/src/components/Rightbar.js
+++ client/src/components/Rightbar.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { Box, Button, ButtonGroup, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, IconButton, TextField } from "@mui/material";
 import { Stack } from "@mui/system";
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import VideoCameraBackIcon from '@mui/icons-material/VideoCameraBack';
 import axios from "axios";
-import { useParams } from "react-router-dom";
+
 
 const Rightbar = (props) => {
-    const { id } = useParams();
     const [ description, setDescription ] = useState('');
     const [ image, setImage ] = useState('');
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
+        console.log('successfully added')
 
         const newPost =  {
             description,
@@ -32,29 +32,35 @@ const Rightbar = (props) => {
         <Box bgcolor='antique white' flex={2} p={2}  sx={{ display: { xs: 'none', sm: 'block'} }}
         >
             <Box position='fixed' mt={8}>
-                <TextField sx={{width: '100%'
-                }}
-                id="outlined-multiline-static"
-                multiline
-                rows={5}
-                placeholder="What's on your mind?"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                />
-                <Stack direction='row' gap={1} marginTop={1}>
-                    <AddPhotoAlternateIcon color="primary"
-                        type='file'
-                        value={image}
-                        onChange={(e) => setImage(e.target.value)}
+                <form onSubmit={onSubmitHandler}>
+                    <TextField sx={{width: '100%'
+                    }}
+                    id="outlined-multiline-static"
+                    multiline
+                    rows={5}
+                    placeholder="What's on your mind?"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
                     />
-                    <VideoCameraBackIcon />
-                </Stack>
-                <ButtonGroup
-                    variant="contained" 
-                    aria-label="outlined primary button group"
-                >
-                    <Button onSubmit={onSubmitHandler}>Post</Button>
-                </ButtonGroup>
+                    <Stack direction='row' gap={1} marginTop={1}>
+                        <IconButton color="primary"
+                                type='file'
+                                value={image}
+                                onChange={(e) => setImage(e.target.value)}
+                                aria-label="add"
+                            >
+                            <AddPhotoAlternateIcon />
+                        </IconButton>
+                        <IconButton color="primary"
+                                type='file'
+                                value={image}
+                                onChange={(e) => setImage(e.target.value)}
+                                aria-label="add">
+                            <VideoCameraBackIcon />
+                        </IconButton>
+                    </Stack>
+                        <Button variant="contained" type="submit">Post</Button>
+                </form>
             </Box>
         </Box>
     );
